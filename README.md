3dd9b68fcf51081586b8892ddbf19e4bcda69d2d
yongqiang <netqyq@gmail.com>
2017-03-31 14:27:50 +0800

    feat. Adds update action the users controller.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Market Place API
 
+market_place is an API service.
+it is completed using TDD.
+
 http://apionrails.icalialabs.com/
 
 
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -53,6 +53,44 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it { should respond_with 422 }
     end
 
+    describe "PUT/PATCH #update" do 
+      context "when is successfully updated" do 
+        before(:each) do 
+          @user = FactoryGirl.create :user
+          patch :update, params: {id: @user.id, user: {email: "abcded@test.com"}, format: :json}
+        end
+
+        it "renders the json representation for the updated user" do 
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:email]).to eql "abcded@test.com"
+        end
+
+        it { should respond_with 200 }
+
+      end
+
+      context "when is not successfully updated" do 
+        before(:each) do 
+          @user = FactoryGirl.create :user
+          patch :update, params: {id: @user.id, user: {email: "bademail"}, format: :json}
+        end
+
+        it "renders an errors json" do 
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response).to have_key(:errors)
+        end
+
+        it "renders the json errors on why the user could not be updated" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:errors][:email]).to include "is invalid"
+        end
+
+        it { should respond_with 422 }
+
+      end
+
+    end
+
   end
 
 
