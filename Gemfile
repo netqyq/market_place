48b7b0e1e77fc74ca6802cbef2728e7beac64b84
yongqiang <netqyq@gmail.com>
2017-03-31 10:15:27 +0800

    Adds devise user model

--- Gemfile
+++ Gemfile
@@ -39,3 +39,11 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :test do
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
+
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
     # We are going to list our resources here
-    
+    scope module: :v1 do
+      # We are going to list our resources here
+      constraints ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      
+      end
+    end
   end
 end
