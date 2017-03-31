ad97d72d0f0579db529497abd6ec5339c22028de
yongqiang <netqyq@gmail.com>
2017-03-31 18:05:15 +0800

    feat. Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,31 @@ RSpec.describe User, type: :model do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:auth_token) }
   it { should be_valid }
 
-
   it { should validate_presence_of(:email) }
+
+  # here have problem, why not should?
   it { should_not validate_uniqueness_of(:email) }
+
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+  # we test the auth_token is unique
+  it { should validate_uniqueness_of(:auth_token)}
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      Devise.stub(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "auniquetoken123"
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
+  end
 
 end
